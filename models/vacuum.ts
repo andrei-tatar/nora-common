353587bd1e8a089780b319913965932bfc671dfd
Nicandros Nic <nicandris@gmail.com>
2020-05-10 15:29:20 +0200

    Removed future states

--- models/vacuum.ts
+++ models/vacuum.ts
@@ -1,10 +1,6 @@
 import { BaseDevice } from './device';
 import { DockState } from './states/dock';
 import { StartStopState } from './states/startstop';
-// import { DockState } from './states/dock';
-// import { LocatorState } from './states/locator';
-// import { ModesState } from './states/modes';
-// import { TogglesState } from './states/toggles';
 
 interface BaseVacuum {
     type: 'vacuum';
